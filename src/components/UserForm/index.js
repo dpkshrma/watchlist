c1f938ea16556c8ec7a4ec7736d983c2a792f44a
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-02 23:05:04 +0530

    Fetch genres from api

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import Select from 'react-select';
+import { Link } from 'react-router-dom';
 import 'react-select/dist/react-select.css';
+import queryString from 'query-string';
 import PlayIcon from './PlayIcon';
+import { TMDB_API_KEY, TMDB_GENRE_URL } from '../config';
 
 const FormWrapper = styled.div`
   display: flex;
@@ -79,29 +82,39 @@ const MOVIE_TYPES = [
   { value: 'rare', label: 'rare' },
 ];
 
-const GENRES = [
-  { value: 'horror', label: 'horror' },
-  { value: 'comedy', label: 'comedy' },
-  { value: 'romantic', label: 'romantic' },
-  { value: 'action', label: 'action' },
+const DEFAULT_GENRES = [
+  { value: 27, label: 'horror' },
+  { value: 35, label: 'comedy' },
+  { value: 10749, label: 'romantic' },
+  { value: 28, label: 'action' },
 ];
 
 const WATCHED_OPTIONS = [
-  { value: 'ignore', label: 'Ignore' },
-  { value: 'include', label: 'Include' },
+  { value: false, label: 'Ignore' },
+  { value: true, label: 'Include' },
 ];
 
 class UserForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      genre: GENRES[0],
-      genres: GENRES,
+      genre: DEFAULT_GENRES[0],
+      genres: DEFAULT_GENRES,
       movieType: MOVIE_TYPES[0],
       movieTypes: MOVIE_TYPES,
-      watchedOption: WATCHED_OPTIONS[0],
+      includeWatched: WATCHED_OPTIONS[0],
       watchedOptions: WATCHED_OPTIONS,
     };
+  }
+  componentWillMount() {
+    this.getGenres()
+      .then(genres => {
+        let genre = null;
+        if (genres.length > 0) {
+          genre = genres[0];
+          this.setState({ genres, genre });
+        }
+      });
   }
   updateGenre = genre => {
     this.setState({ genre });
@@ -109,20 +122,49 @@ class UserForm extends React.Component {
   updateMovieType = movieType => {
     this.setState({ movieType });
   }
-  updateWatchedOption = watchedOption => {
-    this.setState({ watchedOption });
+  updateWatchedOption = includeWatched => {
+    this.setState({ includeWatched });
+  }
+  getGenres = () => {
+    return fetch(`${TMDB_GENRE_URL}?api_key=${TMDB_API_KEY}`)
+      .then(response => response.json())
+      .then(({ genres=[] }) => {
+        return genres.map(({ id, name }) => ({ value: id, label: name }));
+      });
+  }
+  startsWithVowel = (text='') => {
+    if (text.length === 0) {
+      return false;
+    }
+    const [firstChar] = text;
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(firstChar.toLowerCase()) !== -1;
   }
   render() {
+    const {
+      genre={},
+      genres=[],
+      movieType={},
+      movieTypes=[],
+      includeWatched={},
+      watchedOptions=[]
+    } = this.state;
+
+    const listQueryString = queryString.stringify({
+      genreId: genre && genre.value,
+      sort_by: movieType.value,
+      include_watched: includeWatched.value,
+    });
+
     return (
       <FormWrapper>
         <Form>
           <Label>What'd like to watch?</Label>
           <Text>I'd like to watch </Text>
-          {this.state.genre.value && this.state.genre.value.startsWith('a') ? <Text> an </Text> : <Text> a </Text>}
+          {this.startsWithVowel(genre.label) ? <Text> an </Text> : <Text> a </Text>}
           <MultiSelect
             name="genre"
-            value={this.state.genre}
-            options={this.state.genres}
+            value={genre}
+            options={genres}
             onChange={this.updateGenre}
             searchable={false}
           />
@@ -130,24 +172,26 @@ class UserForm extends React.Component {
           <Text>Suggest me some </Text>
           <MultiSelect
             name="movieType"
-            value={this.state.movieType}
-            options={this.state.movieTypes}
+            value={movieType}
+            options={movieTypes}
             onChange={this.updateMovieType}
             searchable={false}
           />
           <Text> ones!</Text><br />
           <MultiSelect
             name="movieType"
-            value={this.state.watchedOption}
-            options={this.state.watchedOptions}
+            value={includeWatched}
+            options={watchedOptions}
             onChange={this.updateWatchedOption}
             searchable={false}
           />
           <Text>the ones I've watched.</Text>
           <br />
-          <Icon>
-            <PlayIcon width="64px" />
-          </Icon>
+          <Link to={`list?${listQueryString}`}>
+            <Icon>
+              <PlayIcon width="64px" />
+            </Icon>
+          </Link>
         </Form>
       </FormWrapper>
     );
---
+++ src/components/config.js
@@ -0,0 +1,11 @@
+const TMDB_API_URL = 'https://api.themoviedb.org/3';
+export const TMDB_DISCOVER_URL = `${TMDB_API_URL}/discover/movie`;
+export const TMDB_API_KEY = '43ebc6d83fb90584067dc3b4f6d8b723';
+export const SORT_MAP = {
+  latest: 'release_date.desc',
+  mostWatched: 'popularity.desc',
+  topRated: 'vote_average.desc',
+  rare: 'release_date.asc',
+};
+
+export const TMDB_GENRE_URL = `${TMDB_API_URL}/genre/movie/list`;
