5fad70b98401ad681075691f6a9676214047b16c
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-07 15:40:02 +0530

    Add URL prefix for production version

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { App, UserForm, SuggestionList, NotFoundPage } from './components';
 
+const urlPrefix = process.env.PUBLIC_URL || '';
+
 export default (
   <App>
     <Switch>
-      <Route exact component={UserForm} path="/" />
-      <Route exact component={SuggestionList} path="/list" />
+      <Route exact component={UserForm} path={`${urlPrefix}/`} />
+      <Route exact component={SuggestionList} path={`${urlPrefix}/list`} />
       <Route component={NotFoundPage} />
     </Switch>
   </App>
