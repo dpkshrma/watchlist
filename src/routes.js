e041fe450d65cb9e6c51db142d6002a95ac86d64
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-12 06:24:15 +0530

    Add route transition animation

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,45 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { spring, AnimatedSwitch } from 'react-router-transition';
 import App from './components/App';
 import { asyncLoad } from './helpers';
+
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  // FIXME: atEnter doesn't seem to work properly :/
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
 
 const UserForm = asyncLoad({
   loader: () => import('./components/UserForm'),
@@ -12,9 +50,14 @@ const SuggestionList = asyncLoad({
 
 export const routes = (
   <App>
-    <Switch>
+    <AnimatedSwitch
+      atEnter={bounceTransition.atEnter}
+      atLeave={bounceTransition.atLeave}
+      atActive={bounceTransition.atActive}
+      mapStyles={mapStyles}
+    >
       <Route exact path="/" component={UserForm} />
       <Route exact path="/list" component={SuggestionList} />
-    </Switch>
+    </AnimatedSwitch>
   </App>
 );
